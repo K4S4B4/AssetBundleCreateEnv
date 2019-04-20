bb79f4d1de45989dee72add9a0b0ff192f3126ad
K4S4B4 <36610547+K4S4B4@users.noreply.github.com>
2019-04-20 23:23:09 +0900

    Initial commit

---
+++ Assets/Editor/ExportAssetBundle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class ExportAssetBundles
+{
+    static string getDirectory()
+    {
+        string assetBundleDirectory = EditorUtility.SaveFolderPanel("Choose a folder to export asset bundles", "", "");
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        return assetBundleDirectory;
+    }
+
+    [MenuItem("**Build AssetBundles**/Build ...")]
+    static void BuildAllAssetBundlesWin()
+    {
+        BuildPipeline.BuildAssetBundles(getDirectory(), BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
